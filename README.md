c36c2da60bacfcd133ab5a1db78926ab5abbbb2f
ospacek <smoby@protonmail.com>
2020-01-28 13:21:58 +0100

    initial project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project Setup
+* `python3 -m venv venv`
+* `source venv/bin/activate`
+* `python setup.py develop`
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='numericke_metody',
+    version='0.01',
+    install_requires=[
+        'numpy==1.18.1',
+        'matplotlib==3.1.2',
+        'jupyter==1.0.0',
+        'notebook==6.0.3',
+    ]
+)
