a292e896204fd81d147a36beb9585ca494332af6
ospacek <smoby@protonmail.com>
2020-01-29 18:47:41 +0100

    add progress and example notebook

--- numericke_metody/slar/gauss_seidel.py
+++ numericke_metody/slar/gauss_seidel.py
@@ -1,23 +1,26 @@
 import numpy as np
 
 
-def gauss_seidel_method(A, b, x0, eps=0.001, max_iter=100):
+def gauss_seidel_method(A, b, x0, eps=0.001, max_iter=100, show_progress=False):
     '''
     Gauss-Seidelova metoda pro řešení soustavy rovnic Ax=b
     Vstupní parametry
     -----------------
-    A ......... matice soustavy
-    b ......... vektor pravé strany
-    x0 ........ vektor počáteční aproximace
-    eps ....... požadovaná přesnost
-    max_iter .. maximální počet iterací
+    A .............. matice soustavy
+    b .............. vektor pravé strany
+    x0 ............. vektor počáteční aproximace
+    eps ............ požadovaná přesnost
+    max_iter ....... maximální počet iterací
+    show_progress .. vypisovat / nevypisovat průběh výpočtu
 
-    Výstupní parametry
-    ------------------
-    x ......... aproximace řešení
-    iters ..... počet iteračních kroků metody
-    norm_err .. euklidovská norma rozdílu posledních dvou iterací
-    progress .. slovník s celým průběhem výpočtu (klíče x_vals, norm_err_vals)
+    Výstupní parametry (slovník)
+    ----------------------------
+    result keys
+        x_approx ....... aproximace řešení
+        norm_err ....... euklidovská norma rozdílu posledních dvou iterací
+        iters .......... počet iteračních kroků metody
+        x_vals ......... aproximace řešení v průběhu výpočtu
+        norm_err_vals .. euklidovská norma rozdílu dvou po sobě jdoucích iterací v průběhu výpočtu
     '''
     D = np.diag(np.diagonal(A))
     U, L = np.triu(A) - D, np.tril(A) - D
@@ -25,12 +28,22 @@ def gauss_seidel_method(A, b, x0, eps=0.001, max_iter=100):
     g = np.dot(np.linalg.inv(L + D), b)
     x_vals, norm_err_vals = list(), [None]
     x = x0
-    for it in range(max_iter):
+    for it in range(1, max_iter + 1):
+        if show_progress:
+            print(f'Iterace: {it}')
+            print(f'x = {x.flatten()}, norm_err = {norm_err_vals[-1]}')
+            print()
         x_vals.append(x)
         x = np.dot(H, x) + g
         norm_err_vals.append(np.linalg.norm(x_vals[-1] - x))
         if norm_err_vals[-1] < eps:
             x_vals.append(x)
             break
-    progress = dict(x_vals=x_vals, norm_err_vals=norm_err_vals)
-    return x_vals[-1], it+1, norm_err_vals[-1], progress
+    result = {
+        'x_approx': x_vals[-1],
+        'norm_err': norm_err_vals[-1],
+        'iters': it,
+        'x_vals': x_vals,
+        'norm_err_vals': norm_err_vals,
+    }
+    return result
--- numericke_metody/utils/visualizations.py
+++ numericke_metody/utils/visualizations.py
@@ -6,6 +6,6 @@ def plot_vals(vals, title):
     Simple plot function
     '''
     plt.figure()
-    plt.plot(vals[1:])
+    plt.plot([x for x in range(1, len(vals) + 1)], vals)
     plt.title(title)
     plt.show()
