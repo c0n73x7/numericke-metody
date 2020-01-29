ac108efdf2f20cdf61b4309bb8f5031412c9aa93
Honza Pulpan <honza@pulpan.net>
2020-01-29 18:33:15 +0100

    Finite difference derivation

---
+++ numericke_metody/derint/derivate.py
@@ -0,0 +1,48 @@
+import numpy as np
+import sympy as sym
+
+def finite_difference(f, x0, h, method='cd'):
+    '''
+    Vypočítá derivaci funkce f v bodě x0 pomocí centrální diference
+    
+    Vstupní parametry
+    -----------------
+    f ........ zadaná funkce (string)
+    x0 ....... bod v kterém chci derivaci f
+    h ........ krok
+    method ... použitá metoda:
+            fd ... pravá diference
+            bd ... levá diference
+            cd ... centrální diference
+    
+    Výstupní parametry
+    ------------------
+    res ....... výsledná hodnota derivace v bodě x0
+    progress .. slovník s celým výpočtem:
+        f_der_sym_x0 .... hodnota symbolické derivace v x0
+        err ............. chyba |přesné-numerické| řešení
+        
+    
+    TODO
+    - vracet podklady pro vykreslení: (???)
+            - původní funkci symbolicky,
+            - její derivaci jako přímku,
+            - numerickou derivaci jako přímku
+    '''
+    x = sym.symbols('x')
+    f_sym = sym.sympify(f)
+    f_der_sym_x0 = sym.diff(f_sym,x).subs(x,x0).evalf()
+    
+    f_x0 = f_sym.subs(x,x0).evalf()
+    f_x0_plus_h = f_sym.subs(x,x0+h).evalf()
+    f_x0_minus_h = f_sym.subs(x,x0-h).evalf()
+    if method == 'fd':
+        res = (f_x0_plus_h-f_x0)/h
+    elif method == 'bd':
+        res = (f_x0-f_x0_minus_h)/h
+    else:
+        res = (f_x0_plus_h-f_x0_minus_h)/h/2
+    
+    progress = dict(f_der_sym_x0 = f_der_sym_x0, err = np.abs(f_der_sym_x0-res)) 
+    
+    return res, progress 
--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='0.01',
     install_requires=[
         'numpy==1.18.1',
+        'sympy==1.5.1',
         'matplotlib==3.1.2',
         'jupyter==1.0.0',
+        'jupyterlab==1.2.6',
         'notebook==6.0.3',
     ]
 )
