e89b80132b43b506d20c0585362a741b78072555
Honza Pulpan <honza@pulpan.net>
2020-01-30 16:22:10 +0100

    Add second derivative

--- numericke_metody/derint/derivate.py
+++ numericke_metody/derint/derivate.py
@@ -75,6 +75,24 @@ def central_difference(f_str, x0, h):
     f = str_to_func(f_str)    
     return (f(x0+h)-f(x0-h))/(2*h)
 
+def second_central_difference(f_str, x0, h):
+    '''
+    Vypočítá druhou derivaci funkce f v bodě x0 pomocí tříbodového vzorce
+    
+    Vstupní parametry
+    -----------------
+    f ........ zadaná funkce (string)
+    x0 ....... bod v kterém chci derivaci f
+    h ........ krok
+    
+    Výstupní parametry
+    ------------------
+    Numerická hodnota druhé derivace fce v bodě x0.
+    
+    '''
+    f = str_to_func(f_str)    
+    return (f(x0+h)-2*f(x0)+f(x0-h))/h**2
+
 def sym_diff(f_str, x0):
     '''
     Vypočte symbolicky hodnotu derivace funkce v bodě.
@@ -90,5 +108,24 @@ def sym_diff(f_str, x0):
     '''
     x = sym.symbols('x')
     f_sym = sym.sympify(f_str)
-    return sym.diff(f_sym,x).subs(x,x0).evalf()
+    return sym.diff(f_sym, x).subs(x, x0).evalf()
+
+def sym_diff2(f_str, x0):
+    '''
+    Vypočte symbolicky hodnotu druhé derivace funkce v bodě.
+    
+    Vstupní parametry
+    -----------------
+    f ........ zadaná funkce (string)
+    x0 ....... bod v kterém chci derivaci f
+    
+    Výstupní parametry
+    ------------------
+    Hodnota druhé derivace fce v bodě x0.
+    '''
+    x = sym.symbols('x')
+    f_sym = sym.sympify(f_str)
+    return sym.diff(f_sym, x, 2).subs(x, x0).evalf()
+
+
 
