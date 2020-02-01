352e43278d6b64b45501f116048be2b5429a5fd8
Honza Pulpan <honza@pulpan.net>
2020-02-01 10:31:29 +0100

    Richardson extrapolation for integrals

--- numericke_metody/derint/__init__.py
+++ numericke_metody/derint/__init__.py
@@ -1,4 +1,4 @@
 from .derivate import (str_to_func, forward_difference, backward_difference,
                        central_difference, second_central_difference, sym_diff, sym_diff2, der_richardson)
-from .integrate import rectangle_integrate, trapezoid_integrate, simpson_integrate, sym_integrate
+from .integrate import rectangle_integrate, trapezoid_integrate, simpson_integrate, sym_integrate, richardson_integrate
 
--- numericke_metody/derint/integrate.py
+++ numericke_metody/derint/integrate.py
@@ -109,3 +109,55 @@ def simpson_integrate(f_str, a, b, N):
     
     return result
 
+def richardson_integrate(f_str, a, b, N, cor_num = 3, method = 'rt'):
+    '''
+    Vypočítá určitý integrál funkce pomocí Richardsonovy extrapolace
+    
+    Vstupní parametry
+    -----------------
+    f_str .... zadaná funkce (string)
+    a ........ dolní mez
+    b ........ horní mez
+    N ........ počet intervalů dělení
+    cor_num .. počet korekcí 
+    method ... použité pravidlo
+            rt ... obdélníkové pravidlo
+            tr ... lichoběžníkové pravidlo
+            sm ... simpsonovo pravidlo
+    
+    Výstupní parametry (slovník)
+    ----------------------------
+    result keys
+        f_int ........ numerická hodnota integrálu fce
+        f_int_vals ... hodnoty použité metody a všech korekcí 
+    '''
+    
+    #hs = [h_init/(2**i) for i in range(N+1)]
+    
+    I = []
+    if method == 'rt':
+        I.append([rectangle_integrate(f_str, a, b, N*(2**i)) for i in range(cor_num+1)])
+        m = 2
+    elif method == 'tr': 
+        I.append([trapezoid_integrate(f_str, a, b, N*(2**i)) for i in range(cor_num+1)])
+        m = 2
+    elif method == 'sm': 
+        I.append([simpson_integrate(f_str, a, b, N*(2**i)) for i in range(cor_num+1)])
+        m = 4
+    else:
+        help(richardson_integrate)
+        result = {
+            'f_int': None,
+            'f_int_vals': None
+        }
+        return result
+        
+    for i in range(cor_num):
+        I.append([I[i][j+1]-(I[i][j+1]-I[i][j])/(1-2**(m*(i+1))) for j in range(len(I[i])-1)])
+    
+    result = {
+        'f_int': I[-1][-1],
+        'f_int_vals': I
+    }
+    
+    return result
