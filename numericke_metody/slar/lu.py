d2a9277050054dae63ea288a732f2ea2aea1b037
ospacek <smoby@protonmail.com>
2020-02-02 22:31:25 +0100

    lu method

---
+++ numericke_metody/slar/lu.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numericke_metody.factorization import lu as lu_factorization
+
+
+def lu(A, b):
+    '''
+    Metoda LU rozkladu pro řešení soustavy Ax = b
+
+    Vstupní parametry
+    -----------------
+    A .............. matice soustavy
+    b .............. vektor pravé strany
+
+    Výstupní parametry
+    ------------------
+    x .............. vektor řešení soustavy
+    '''
+    L, U = lu_factorization(A)
+    n = A.shape[0]
+    y = np.copy(b)
+    for i in range(n):
+        for j in range(i):
+            y[i] = y[i] - L[i, j] * y[j]
+    x = np.copy(y)
+    for i in reversed(range(n)):
+        for j in range(i+1, n):
+            x[i] -= U[i, j] * x[j]
+        x[i] /= U[i, i]
+    return x
