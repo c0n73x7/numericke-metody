2b59c253f4387375122ef2bcde651988ac501b0f
ospacek <smoby@protonmail.com>
2020-02-03 22:14:34 +0100

    simple iteration - update

---
+++ numericke_metody/nonlinear_equations/__init__.py
@@ -0,0 +1,1 @@
+from .simple_iteration import simple_iteration
--- numericke_metody/nonlinear_equations/simple_iteration.py
+++ numericke_metody/nonlinear_equations/simple_iteration.py
@@ -1,13 +1,40 @@
 import numpy as np
 
 
-def simple_iteration(phi, x0, eps=0.001, max_iter=100, show_progress=False):
-    x_vals, abs_err_vals = list(), list()
+def simple_iteration(phi, x0, eps=0.001, max_iter=300, show_progress=False):
+    '''
+    The Method of Simple Iteration
+
+    Input Params
+    ------------
+    phi ............ TODO
+    x0 ............. TODO
+    eps ............ TODO
+    max_iter ....... TODO
+    show_progress .. TODO
+
+    Output Params
+    -------------
+    result -> dict
+        x_approx ...... TODO
+        abs_err ....... TODO
+        iters ......... TODO
+        x_vals ........ TODO
+        abs_err_vals .. TODO
+    '''
+    x_vals, abs_err_vals = list(), [None]
     x = x0
-    for it in range(max_iter):
+    if show_progress:
+        print(f'Initial approximation x0 = {x}')
+        print()
+    for it in range(1, max_iter+1):
         x_vals.append(x)
         x = phi(x)
         abs_err_vals.append(np.abs(x - x_vals[-1]))
+        if show_progress:
+            print(f'Iteration: {it}')
+            print(f'x = {x}, abs_err = {abs_err_vals[-1]}')
+            print()
         if abs_err_vals[-1] < eps:
             x_vals.append(x)
             break
