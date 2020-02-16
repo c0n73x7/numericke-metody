a5e38edb2c05f0ab1d74de4f1e8fc099bafce228
ospacek <smoby@protonmail.com>
2020-02-16 16:01:15 +0100

    secant method

--- numericke_metody/nonlin_equations/__init__.py
+++ numericke_metody/nonlin_equations/__init__.py
@@ -2,3 +2,4 @@ from .bisection import bisection
 from .simple_iteration import simple_iteration
 from .regula_falsi import regula_falsi
 from .newton import newton, modified_newton
+from .secant import secant
