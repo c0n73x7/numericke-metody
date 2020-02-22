d5eb7bcc4d3315a3dc0c59d56eb1c734b943bcd5
Ondřej Špaček <ospacek@protonmail.com>
2020-02-22 23:04:08 +0100

    nonlineq visualization (#11)

--- numericke_metody/utils/visualizations.py
+++ numericke_metody/utils/visualizations.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -24,4 +25,20 @@ def multiplot_vals(xs, vals, title, xscale='linear', yscale='linear', fsize=(12,
     plt.title(title)
     plt.legend()
     plt.show()
-    
+
+
+def nonlineqs_plot(f, vals, x_lims, title, figsize=(10, 6)):
+    '''
+    Plot for nonlinear equations
+    '''
+    x1 = np.linspace(x_lims[0], x_lims[1], int(np.abs(x_lims[1] - x_lims[0]) * 100))
+    plt.figure(figsize=figsize)
+    plt.plot(x1, f(x1), linewidth=2)
+    plt.plot(vals, f(np.array(vals)), 'om', alpha=0.4)
+    plt.plot(vals[-1], f(vals[-1]), 'or')
+    plt.xlim(x_lims)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.title(title)
+    plt.show()
